fda782e39b82ea48a46ab442116b2b924aca948c
raydc00 <67373943+aprentix@users.noreply.github.com>
2024-10-05 12:01:22 +0200

    INitial commit

---
+++ Exosky/Nodes/menu.gd
@@ -0,0 +1,14 @@
+extends MarginContainer
+
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
