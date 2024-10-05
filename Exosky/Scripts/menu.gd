8549774038c68e2b3ad410be35ffdad488e7b35d
raydc00 <67373943+aprentix@users.noreply.github.com>
2024-10-05 15:42:26 +0200

    Btn_select_menu_2

--- Exosky/Scripts/menu.gd
+++ Exosky/Scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-    get_tree().change_scene_to_file("res://Scenes/Game3D.tscn")
+    get_tree().change_scene_to_file("res://Scenes/Menu_select_exoplanet.tscn")
 
 func _on_quit_pressed() -> void:
     get_tree().quit()
