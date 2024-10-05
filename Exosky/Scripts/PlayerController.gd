dd2d711897443f0726b17d091c3ef2a9c62b35d7
notMySenpai <enriquegarridosanchezinf@gmail.com>
2024-10-05 15:35:45 +0200

    Commit

--- Exosky/Scripts/PlayerController.gd
+++ Exosky/Scripts/PlayerController.gd
@@ -1,48 +1,31 @@
 extends Node3D
-#
-## Sensibilidad del ratón
-#var mouse_sensitivity = 0.3
-#
-## Velocidad de movimiento
-#var speed = 10
-#
-## Variables para la rotación de la cámara
-#var yaw = 0 # Rotación en el eje Y
-#var pitch = 0 # Rotación en el eje X
+
+@export var mouse_sensitivity = 0.3
+var speed = 10
+var YAngle = 0 # Rotación en el eje Y
+var XAngle = 0 # Rotación en el eje X
 #
 ## La cámara
-#onready var camera = $Camera
-#
-## Gravedad
-#var gravity = -9.8
-#
-## Velocidad de salto
-#var jump_speed = 5
-#
-## Velocidad del jugador
-#var velocity = Vector3()
-#
-## Para detectar si está en el suelo
-#var is_on_ground = false
-#
-#func _ready():
-    ## Oculta el cursor y bloquea el ratón al centro de la pantalla
-    #Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#
-#func _input(event):
-    ## Detecta la entrada del ratón
-    #if event is InputEventMouseMotion:
-        #yaw -= event.relative_x * mouse_sensitivity
-        #pitch -= event.relative_y * mouse_sensitivity
-#
-        ## Limitar la rotación en el eje X (pitch) para evitar giros completos
-        #pitch = clamp(pitch, -90, 90)
-#
-        ## Aplica la rotación al nodo principal (rotación en Y)
-        #rotation_degrees.y = yaw
-        ## Aplica la rotación en X a la cámara (rotación en X)
-        #camera.rotation_degrees.x = pitch
-#
+@onready var camera = %Camera
+
+func _ready():
+    # Oculta el cursor y bloquea el ratón al centro de la pantalla
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    
+func _input(event):
+    # Detecta la entrada del ratón
+    if event is InputEventMouseMotion:
+        YAngle -= event.relative.x * mouse_sensitivity
+        XAngle -= event.relative.y * mouse_sensitivity
+
+        # Limitar la rotación en el eje X (pitch) para evitar giros completos
+        XAngle = clamp(XAngle, -90, 90)
+
+        # Aplica la rotación al nodo principal (rotación en Y)
+        rotation_degrees.y = YAngle
+        # Aplica la rotación en X a la cámara (rotación en X)
+        camera.rotation_degrees.x = XAngle
+
 #func _process(delta):
     #handle_movement(delta)
 #
@@ -76,4 +59,4 @@ extends Node3D
 #
     ## Mueve al jugador
     #velocity = move_and_slide(velocity, Vector3.UP)
-    #is_on
+    #is_on_ground = is_on_floor()
