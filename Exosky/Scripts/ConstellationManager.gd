404de1226e875d7a6b9a4dfcccb7b2c3393c1f75
notMySenpai <enriquegarridosanchezinf@gmail.com>
2024-10-06 09:35:10 +0200

    summit

--- Exosky/Scripts/ConstellationManager.gd
+++ Exosky/Scripts/ConstellationManager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var lastPos:Vector3
 var currentPoints:Array[Vector3]=[]
 var constellation:Array[Node]=[]
 @onready var meshLine = preload("res://Scenes/line.tscn")
@@ -14,18 +15,21 @@ func _process(_delta: float) -> void:
     pass
 
 func add_Point(vPos:Vector3):
-    cleanup()
-    currentPoints.append(vPos);
-    draw_current_constellation()
+    if(lastPos==vPos): return
+    constellation.append(createLine(lastPos,vPos,Color.WHITE,50))
+    #cleanup()
+    #currentPoints.append(vPos);
+    lastPos=vPos
+    #draw_current_constellation()
     
-func clear_points(vPos:Vector3):
+func clear_points():
     cleanup()
     currentPoints.clear()
     
-func draw_current_constellation():
-    if(currentPoints.size()>=2):
-        for i in range(currentPoints.size() - 1):  # i tomará los valores de 0 a 4
-            constellation.append(createLine(currentPoints[i],currentPoints[i+1],Color.WHITE,50))
+#func draw_current_constellation():
+    #if(currentPoints.size()>=2):
+        #for i in range(currentPoints.size() - 1):  # i tomará los valores de 0 a 4
+            #constellation.append(createLine(currentPoints[i],currentPoints[i+1],Color.WHITE,50))
 
 func cleanup():
     for x in constellation:
@@ -35,7 +39,7 @@ func cleanup():
 func createLine(a, b, color, thickness) -> Node:
     var line = meshLine.instantiate()
     add_child(line)
-    line.scale = Vector3(0.01*thickness, 0.01*thickness, a.distance_to(b))
+    line.scale = Vector3(0.01*thickness, 0.01*thickness, a.distance_to(b)/2)
     line.look_at_from_position((a+b)/2, b, Vector3.UP)
     return line
     
